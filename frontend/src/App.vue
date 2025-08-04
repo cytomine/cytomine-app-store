dfa35b531d378e9ce3a5d58c6137b5cce78b39fb
Ba Thien LE <bathien.le.pro@gmail.com>
2025-08-04 12:37:23 +0200

    feat: add Bulma and Buefy

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="has-text-warning">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p class="mb-6">Margin bottom</p>
+  <b-button class="mb-2 is-primary has-text-grey is-size-2 is-align-content-end">Click Me</b-button>
 </template>
 
 <style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,17 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import 'bulma/css/bulma.css';
+
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(Buefy, { defaultIconPack: 'fas' });
 app.use(router);
 
 app.mount('#app');
