844e5122d07eef8fb06787717472255b8305fd38
Ba Thien LE <bathien.le.pro@gmail.com>
2025-08-04 12:05:34 +0200

    feat: add eslint rules

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,8 +1,8 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginPlaywright from 'eslint-plugin-playwright'
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import pluginPlaywright from 'eslint-plugin-playwright';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -12,21 +12,46 @@ import pluginPlaywright from 'eslint-plugin-playwright'
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{ts,vue}'],
+    rules: {
+      'array-bracket-spacing': ['error', 'never'],
+      'brace-style': ['error', '1tbs'],
+      'camelcase': ['error', { allow: ['$_veeValidate'] }],
+      'curly': ['error', 'all'],
+      'eqeqeq': ['error', 'smart'],
+      'indent': ['error', 2, { 'SwitchCase': 1, 'ignoredNodes': ['TemplateLiteral'] }],
+      'keyword-spacing': ['error'],
+      'no-console': ['off'],
+      'no-redeclare': ['error'],
+      'no-undef': ['error'],
+      'no-unused-vars': ['error'],
+      'no-var': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+      'semi': ['error', 'always'],
+      'space-before-blocks': ['error', 'always'],
+      'space-before-function-paren': ['error', {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      }],
+      'space-infix-ops': ['error'],
+      'space-in-parens': ['error'],
+    },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
   },
-)
+);
