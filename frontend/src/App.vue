dce16fb6adeb7333d8ffceb5fdff5f1ae03ddacb
Ba Thien LE <bathien.le.pro@gmail.com>
2025-08-04 14:40:55 +0200

    feat: add placeholder navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
-<script setup lang="ts"></script>
-
 <template>
+  <AppNavbar />
   <h1 class="has-text-warning">{{ $t('hello-world') }}</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
@@ -11,3 +10,7 @@
     <b-icon icon="plus" /> Click Me
   </b-button>
 </template>
+
+<script setup lang="ts">
+import AppNavbar from '@/components/navbar/AppNavbar.vue';
+</script>
