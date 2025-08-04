a08f3fbd8d2b1a1d1d17357fc60ec6aa632424e2
Ba Thien LE <bathien.le.pro@gmail.com>
2025-08-04 15:34:24 +0200

    chore: remove boilerplate

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,6 @@
   <div>
     <AppNavbar />
     <h1 class="has-text-warning">{{ $t('hello-world') }}</h1>
-    <p>
-      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-      documentation
-    </p>
-    <p class="mb-6">Margin bottom</p>
-    <b-button class="mb-2 is-primary has-text-grey is-size-2 is-align-content-end">
-      <b-icon icon="plus" /> Click Me
-    </b-button>
   </div>
 </template>
 
