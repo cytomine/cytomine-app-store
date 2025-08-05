e5b4ecbbf21c7a36086910a377e767966783758d
sidhamed <siddiqhamed2017@gmail.com>
2025-08-05 17:03:20 +0200

    chore: added and modified test files

---
+++ backend/src/test/java/be/cytomine/appstore/services/TaskValidationServiceTest.java
@@ -0,0 +1,114 @@
+package be.cytomine.appstore.services;
+
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import be.cytomine.appstore.dto.inputs.task.UploadTaskArchive;
+import be.cytomine.appstore.exceptions.ValidationException;
+import be.cytomine.appstore.models.task.Task;
+import be.cytomine.appstore.repositories.TaskRepository;
+import be.cytomine.appstore.services.TaskValidationService;
+import be.cytomine.appstore.utils.TaskUtils;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskValidationServiceTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskValidationService taskValidationService;
+
+    private static Task task;
+
+    private static UploadTaskArchive archive;
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+        task = TaskUtils.createTestTask(false);
+        archive = TaskUtils.createTestUploadTaskArchive();
+    }
+
+    @DisplayName("Successfully return void when no duplicates")
+    @Test
+    public void checkIsNotDuplicateShouldReturnVoid() throws Exception {
+        String namespace = archive.getDescriptorFileAsJson().get("namespace").textValue();
+        String version = archive.getDescriptorFileAsJson().get("version").textValue();
+        when(taskRepository.findByNamespaceAndVersion(namespace, version)).thenReturn(null);
+
+        assertDoesNotThrow(() -> taskValidationService.checkIsNotDuplicate(archive));
+        verify(taskRepository, times(1)).findByNamespaceAndVersion(namespace, version);
+    }
+
+    @DisplayName("Successfully throw 'ValidationException' when duplicates")
+    @Test
+    public void checkIsNotDuplicateShouldThrowValidationException() {
+        String namespace = archive.getDescriptorFileAsJson().get("namespace").textValue();
+        String version = archive.getDescriptorFileAsJson().get("version").textValue();
+        when(taskRepository.findByNamespaceAndVersion(namespace, version)).thenReturn(task);
+
+        ValidationException exception = assertThrows(
+            ValidationException.class,
+            () -> taskValidationService.checkIsNotDuplicate(archive)
+        );
+        assertEquals("Task already exists.", exception.getMessage());
+        verify(taskRepository, times(1)).findByNamespaceAndVersion(namespace, version);
+    }
+
+    @DisplayName("Successfully return void when image is valid")
+    @Test
+    public void validateImageShouldReturnVoid() throws Exception {
+        assertDoesNotThrow(() -> taskValidationService.validateImage(archive));
+    }
+
+    @DisplayName("Fail to validate image and throw 'ValidationException'")
+    @Test
+    public void validateImageShouldThrowValidationException() throws Exception {
+        UploadTaskArchive missing = TaskUtils.createTestUploadTaskArchive();
+        missing.setDockerImage(Files.createTempFile("docker-image", ".tar").toFile());
+
+        ValidationException exception = assertThrows(
+            ValidationException.class,
+            () -> taskValidationService.validateImage(missing)
+        );
+        assertEquals("image is not invalid manifest is missing", exception.getMessage());
+    }
+
+    @DisplayName("Successfully validate the descriptor")
+    @Test
+    public void validateDescriptorFileShouldReturnVoid() throws Exception {
+        assertDoesNotThrow(() -> taskValidationService.validateDescriptorFile(archive));
+    }
+
+    @DisplayName("Fail to validate the descriptor throw 'ValidationException'")
+    @Test
+    public void validateDescriptorFileShouldThrowValidationException() throws Exception {
+        UploadTaskArchive invalid = TaskUtils.createTestUploadTaskArchive();
+        JsonNode descriptor = invalid.getDescriptorFileAsJson();
+
+        ObjectNode node = (ObjectNode) descriptor;
+        node.put("name", "a");
+        invalid.setDescriptorFileAsJson(descriptor);
+
+        ValidationException exception = assertThrows(
+            ValidationException.class,
+            () -> taskValidationService.validateDescriptorFile(invalid)
+        );
+        assertEquals("schema validation failed for descriptor.yml", exception.getMessage());
+    }
+}
---
+++ backend/src/test/java/be/cytomine/appstore/utils/TaskUtils.java
@@ -0,0 +1,131 @@
+package be.cytomine.appstore.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Set;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import be.cytomine.appstore.dto.inputs.task.UploadTaskArchive;
+import be.cytomine.appstore.handlers.StorageData;
+import be.cytomine.appstore.handlers.StorageDataEntry;
+import be.cytomine.appstore.handlers.StorageDataType;
+import be.cytomine.appstore.models.task.Author;
+import be.cytomine.appstore.models.task.Parameter;
+import be.cytomine.appstore.models.task.ParameterType;
+import be.cytomine.appstore.models.task.Task;
+import be.cytomine.appstore.models.task.Type;
+import org.springframework.core.io.ClassPathResource;
+
+public class TaskUtils {
+    public static UploadTaskArchive createTestUploadTaskArchive() throws IOException {
+        File descriptorFile = new ClassPathResource("artifacts/descriptor.yml").getFile();
+
+        // Create a copy because it will be deleted after the upload process
+        Path original = new ClassPathResource("artifacts/image.tar").getFile().toPath();
+        Path copy = Path.of("src/test/resources/artifacts/docker-test-image.tar");
+        Files.copy(original, copy, StandardCopyOption.REPLACE_EXISTING);
+        File dockerImage = copy.toFile();
+        dockerImage.deleteOnExit();
+
+        return new UploadTaskArchive(descriptorFile, dockerImage);
+    }
+
+    public static Author createTestAuthor() {
+        Author author = new Author();
+        author.setFirstName("Cytomine");
+        author.setLastName("ULiege");
+        author.setOrganization("University of Liege");
+        author.setEmail("cytomine@uliege.be");
+        author.setContact(true);
+
+        return author;
+    }
+
+    public static Parameter createTestInput(String name, boolean binaryType) {
+        Type type = new Type();
+        type.setCharset("UTF-8");
+
+        Parameter input = new Parameter();
+        input.setName(name);
+        input.setDisplayName("Input");
+        input.setDescription("Input description");
+        input.setOptional(false);
+        input.setType(type);
+        input.setParameterType(ParameterType.INPUT);
+
+        return input;
+    }
+
+    public static Parameter createTestOutput(String name, boolean binaryType) {
+        Type type = new Type();
+        type.setCharset("UTF-8");
+
+        Parameter output = new Parameter();
+        output.setName(name);
+        output.setDisplayName("Output");
+        output.setDescription("output description");
+        output.setOptional(false);
+        output.setType(type);
+        output.setParameterType(ParameterType.OUTPUT);
+
+        return output;
+    }
+
+    public static Task createTestTask(boolean binaryType) {
+        Task task = new Task();
+        task.setIdentifier(UUID.randomUUID());
+        task.setNamespace("namespace");
+        task.setVersion("version");
+        task.setStorageReference("storageReference");
+        task.setDescription("Test Task Description");
+        task.setAuthors(Set.of(createTestAuthor()));
+        task.setParameters(Set.of(createTestInput("name", binaryType), createTestOutput("out", binaryType)));
+
+        return task;
+    }
+
+    public static Task createTestTaskWithMultipleIO() {
+        Task task = new Task();
+        task.setIdentifier(UUID.randomUUID());
+        task.setNamespace("namespace");
+        task.setVersion("version");
+        task.setStorageReference("storageReference");
+        task.setDescription("Test Task Description");
+        task.setAuthors(Set.of(createTestAuthor()));
+        task.setParameters(Set.of(createTestInput("name 1", false),
+            createTestInput("name 2", false),
+            createTestOutput("out 1", false),
+            createTestOutput("out 2", false)));
+
+        return task;
+    }
+
+
+    public static StorageData createTestStorageData(String parameterName, String storageId) throws IOException {
+        File data = File.createTempFile("data", null);
+        data.deleteOnExit();
+
+        return new StorageData(new StorageDataEntry(data, parameterName, storageId, StorageDataType.FILE));
+    }
+
+    public static byte[] createFakeOutputsZip(String... names) throws IOException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             ZipOutputStream zos = new ZipOutputStream(baos)) {
+
+            for (String name : names) {
+                zos.putNextEntry(new ZipEntry(name));
+                zos.write("42".getBytes());
+                zos.closeEntry();
+            }
+
+            zos.finish();
+            return baos.toByteArray();
+        }
+    }
+}
