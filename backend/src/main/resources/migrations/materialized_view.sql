f3b831e6a6121c620baa556e270b274ba898059e
sidhamed <siddiqhamed2017@gmail.com>
2025-08-06 15:57:21 +0200

    fix: handle null values in materialized view search_vector

--- backend/src/main/resources/migrations/materialized_view.sql
+++ backend/src/main/resources/migrations/materialized_view.sql
@@ -10,7 +10,17 @@ SELECT t.name,
        a.last_name,
        a.organization,
        a.email,
-       to_tsvector('english', t.name || ' ' || t.namespace || ' ' || t.name_short || ' ' || t.description || ' ' || t.image_name|| ' ' || t.version || ' ' || a.first_name || ' ' || a.last_name || ' ' || a.organization || ' ' || a.email) AS search_vector
+       to_tsvector('english',
+                   COALESCE(t.name, '') || ' ' ||
+                   COALESCE(t.namespace, '') || ' ' ||
+                   COALESCE(t.name_short, '') || ' ' ||
+                   COALESCE(t.description, '') || ' ' ||
+                   COALESCE(t.image_name, '') || ' ' ||
+                   COALESCE(t.version, '') || ' ' ||
+                   COALESCE(a.first_name, '') || ' ' ||
+                   COALESCE(a.last_name, '') || ' ' ||
+                   COALESCE(a.organization, '') || ' ' ||
+                   COALESCE(a.email, '')) AS search_vector
 FROM task t
          JOIN task_authors ta ON t.identifier = ta.task_identifier
          JOIN author a ON ta.authors_id = a.id;
