4915842a60ecf38bc02be256fc8279ff908bc514
sidhamed <siddiqhamed2017@gmail.com>
2025-08-06 15:56:09 +0200

    fix: correct syntax error in materialized view definition

--- backend/src/main/resources/migrations/materialized_view.sql
+++ backend/src/main/resources/migrations/materialized_view.sql
@@ -10,7 +10,7 @@ SELECT t.name,
        a.last_name,
        a.organization,
        a.email,
-       to_tsvector('english', t.name || ' ' || t.namespace || ' ' t.name_short || ' ' || t.description || ' ' || t.image_name|| ' ' || t.version || ' ' || a.first_name || ' ' || a.last_name || ' ' || a.organization || ' ' || a.email) AS search_vector
+       to_tsvector('english', t.name || ' ' || t.namespace || ' ' || t.name_short || ' ' || t.description || ' ' || t.image_name|| ' ' || t.version || ' ' || a.first_name || ' ' || a.last_name || ' ' || a.organization || ' ' || a.email) AS search_vector
 FROM task t
          JOIN task_authors ta ON t.identifier = ta.task_identifier
          JOIN author a ON ta.authors_id = a.id;
