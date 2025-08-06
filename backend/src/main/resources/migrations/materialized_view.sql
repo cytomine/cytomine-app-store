3d7b9ac37278e5f906602e8f335d6daf6117542b
sidhamed <siddiqhamed2017@gmail.com>
2025-08-06 16:32:08 +0200

    feat: update materialized view with pg_trgm extension, full-text search, and fuzzy search support

--- backend/src/main/resources/migrations/materialized_view.sql
+++ backend/src/main/resources/migrations/materialized_view.sql
@@ -1,5 +1,6 @@
+-- 1. create a full-text search extension
 CREATE EXTENSION pg_trgm;
-
+-- 2. create a materialized view for search
 create materialized view search as
 SELECT t.name,
        t.namespace,
@@ -11,6 +12,7 @@ SELECT t.name,
        a.last_name,
        a.organization,
        a.email,
+ -- 3. full-text search column
        to_tsvector('english',
                    COALESCE(t.name, '') || ' ' ||
                    COALESCE(t.namespace, '') || ' ' ||
@@ -21,7 +23,8 @@ SELECT t.name,
                    COALESCE(a.first_name, '') || ' ' ||
                    COALESCE(a.last_name, '') || ' ' ||
                    COALESCE(a.organization, '') || ' ' ||
-                   COALESCE(a.email, '')) AS search_vector
+                   COALESCE(a.email, '')) AS search_vector ,
+ -- 4. fuzzy search column for misspelled words
        COALESCE(t.name, '') || ' ' ||
        COALESCE(t.namespace, '') || ' ' ||
        COALESCE(t.name_short, '') || ' ' ||
@@ -33,12 +36,12 @@ SELECT t.name,
 FROM task t
          JOIN task_authors ta ON t.identifier = ta.task_identifier
          JOIN author a ON ta.authors_id = a.id;
-
+-- 5. a description
 comment on materialized view search is 'for search';
-
+-- 6. change owner
 alter materialized view search owner to appstore;
-
+-- 7. create a generalized inverted index
 CREATE INDEX idx_search_vector ON search USING GIN(search_vector);
-
+-- 8. another one for the fuzzy search
 CREATE INDEX idx_fuzzy_search ON search USING GIN(fuzzy_search_text gin_trgm_ops);
 
