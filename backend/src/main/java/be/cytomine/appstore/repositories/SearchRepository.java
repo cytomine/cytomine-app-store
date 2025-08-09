3f613949618c9f13bbbe333cd50af1a2db769c47
sidhamed <siddiqhamed2017@gmail.com>
2025-08-09 18:32:06 +0200

    feat: add SearchRepository with advanced search query support

---
+++ backend/src/main/java/be/cytomine/appstore/repositories/SearchRepository.java
@@ -0,0 +1,21 @@
+package be.cytomine.appstore.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import be.cytomine.appstore.models.Search;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface SearchRepository extends JpaRepository<Search, UUID> {
+    @Query(value =
+        "SELECT *  " +
+        "FROM search " +
+        "WHERE search_vector @@ to_tsquery('english', :fullTextQuery) " +
+        "OR word_similarity(:fuzzyQuery, fuzzy_search_text) > 0.2 " +
+        "ORDER BY ts_rank(search_vector, to_tsquery('english', :fullTextQuery)) DESC",
+        nativeQuery = true)
+    List<Search> findByAdvancedSearch(@Param("fullTextQuery") String fullTextQuery, @Param("fuzzyQuery") String fuzzyQuery);
+}
