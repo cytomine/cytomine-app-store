d1061f10f19dc9c55d8f3b10da85174a0ab0b0f3
sidhamed <siddiqhamed2017@gmail.com>
2025-08-12 15:03:32 +0200

    chore: remove unused error classes and redundant builder methods in ErrorBuilder

--- backend/src/main/java/be/cytomine/appstore/dto/responses/errors/ErrorBuilder.java
+++ backend/src/main/java/be/cytomine/appstore/dto/responses/errors/ErrorBuilder.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import be.cytomine.appstore.dto.responses.errors.details.BaseErrorDetails;
 import be.cytomine.appstore.dto.responses.errors.details.BatchError;
-import be.cytomine.appstore.dto.responses.errors.details.EmptyErrorDetails;
-import be.cytomine.appstore.dto.responses.errors.details.MultipleErrors;
-import be.cytomine.appstore.dto.responses.errors.details.ParamRelatedError;
 import be.cytomine.appstore.dto.responses.errors.details.ServerError;
 
 public class ErrorBuilder {
@@ -17,7 +14,7 @@ public class ErrorBuilder {
      * @param code The error code
      */
     public static AppStoreError build(ErrorCode code) {
-        return build(code, new EmptyErrorDetails());
+        return build(code, new BaseErrorDetails());
     }
 
     public static AppStoreError build(ErrorCode code, BaseErrorDetails details) {
@@ -34,40 +31,9 @@ public class ErrorBuilder {
         return new AppStoreError(container.code, message, details);
     }
 
-    /***
-     * Build an error message for errors originating from a batch request
-     *
-     * @param errors list of underlying errors, each error corresponds to an item of
-     *               the batch
-     */
-    public static AppStoreError buildBatchError(List<AppStoreError> errors) {
-        ErrorCode code = ErrorCode.INTERNAL_GENERIC_BATCH_ERROR;
-        return build(code, new BatchError(errors));
-    }
-
     public static AppStoreError buildSchemaValidationError(List<AppStoreError> errors) {
         ErrorCode code = ErrorCode.INTERNAL_SCHEMA_VALIDATION_ERROR;
         return build(code, new BatchError(errors));
-    }
-
-    public static AppStoreError buildMultipleErrors(List<AppStoreError> errors) {
-        ErrorCode code = ErrorCode.INTERNAL_MULTIPLE_ERRORS;
-        return build(code, new MultipleErrors(errors));
-    }
-
-    /**
-     * Build an error originating from a batch param
-     *
-     * @param code        top-level error code
-     * @param paramName   param name
-     * @param description param error description
-     */
-    public static AppStoreError buildParamRelatedError(
-        ErrorCode code,
-        String paramName,
-        String description
-    ) {
-        return build(code, new ParamRelatedError(paramName, description));
     }
 
     /**
