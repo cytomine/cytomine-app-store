14ed536ff074a27bc38e4cbc23c74158cc81d001
Ba Thien LE <bathien.le.pro@gmail.com>
2025-08-14 10:23:35 +0200

    feat: add storage navigation

--- frontend/src/components/navbar/AppNavbar.vue
+++ frontend/src/components/navbar/AppNavbar.vue
@@ -1,18 +1,17 @@
 <template>
   <b-navbar class="is-light">
     <template #brand>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+      <b-navbar-item tag="router-link" to="/">
         <img src="https://raw.githubusercontent.com/buefy/buefy/dev/packages/docs/public/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma" />
+          alt="Cytomine App Store" />
       </b-navbar-item>
     </template>
+
     <template #start>
-      <b-navbar-item href="#"> Home </b-navbar-item>
-      <b-navbar-item href="#"> Documentation </b-navbar-item>
-      <b-navbar-dropdown label="Info">
-        <b-navbar-item href="#"> About </b-navbar-item>
-        <b-navbar-item href="#"> Contact </b-navbar-item>
-      </b-navbar-dropdown>
+      <b-navbar-item tag="router-link" to="/storage">
+        <i class="fas fa-download"></i>
+        {{ $t('storage') }}
+      </b-navbar-item>
     </template>
 
     <template #end>
