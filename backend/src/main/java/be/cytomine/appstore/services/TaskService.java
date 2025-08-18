d94b8d29f020bc4627a1ad63aafcec154534064e
sidhamed <siddiqhamed2017@gmail.com>
2025-08-18 18:50:14 +0200

    refactor: remove unused import from TaskService

--- backend/src/main/java/be/cytomine/appstore/services/TaskService.java
+++ backend/src/main/java/be/cytomine/appstore/services/TaskService.java
@@ -6,7 +6,6 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
