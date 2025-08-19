72d51306e5352f91926caac9887d823cd0aa3dbf
sidhamed <siddiqhamed2017@gmail.com>
2025-08-19 10:07:44 +0200

    refactor: remove unnecessary whitespace in AppstoreApplicationTests

--- backend/src/test/java/be/cytomine/appstore/AppstoreApplicationTests.java
+++ backend/src/test/java/be/cytomine/appstore/AppstoreApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppstoreApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {}
 
 }
